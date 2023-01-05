483462e25ef10ae00e6abd8ba0bd7b9d6defc4e5
Nick Mackenzie <mackenzie.nick@outlook.com>
2023-01-04 20:38:07 -0500

    asdf

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,14 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
+    <header>
+ <div  className={clsx('hero hero--primary', styles.heroImage)}>
+  <div className={styles.heroText}>
+    <h1>I am John Doe</h1>
+    <p>And I'm a Photographer</p>
+    <button>Hire me</button>
+  </div>
+</div> 
     </header>
   );
 }
